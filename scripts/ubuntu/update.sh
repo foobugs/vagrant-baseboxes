0aeb34470537862954fd655d17367a23863b1b82
René Oelke <rene.oelke@gmail.com>
2014-07-03 12:20:51 +0200

    Removed --only-upgrade option; added updates for Ubuntu 12.04.

--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -2,4 +2,21 @@
 
 apt-get update
 apt-get upgrade --assume-yes
-apt-get install --only-upgrade --assume-yes linux-server linux-headers-server
+apt-get upgrade --assume-yes linux-server linux-headers-server
+
+MAJOR_VERSION=""
+
+# https://help.ubuntu.com/community/CheckingYourUbuntuVersion
+if [ lsb_release > /dev/null 2>&1 ]
+then
+    # Get the major version (like "12" from the version string (like "12.04")
+    MAJOR_VERSION="$(lsb_release -r | cut -f 2)"
+fi
+
+case "$MAJOR_VERSION" in 
+    "12.04") 
+        apt-get install --assume-yes linux-image-generic-lts-saucy linux-headers-generic-lts-saucy
+        ;;
+    *)
+        ;;
+esac
