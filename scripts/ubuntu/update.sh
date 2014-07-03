78e6ba935583d9922264bd1fbfcb8318df8a7f19
René Oelke <rene.oelke@gmail.com>
2014-07-03 10:23:01 +0200

    Removed commented unused code.

--- scripts/ubuntu/update.sh
+++ scripts/ubuntu/update.sh
@@ -3,14 +3,3 @@
 apt-get update
 apt-get upgrade --assume-yes
 apt-get install --only-upgrade --assume-yes linux-server linux-headers-server
-
-# ensure the correct kernel headers are installed
-#apt-get -y install linux-headers-$(uname -r)
-
-# update package index on boot
-#cat <<EOF > /etc/init/refresh-apt.conf
-#description "update package index"
-#start on networking
-#task
-#exec /usr/bin/apt-get update
-#EOF
